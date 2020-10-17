3c99980482eb29439776cd5a24e7000e8ce2d2e3
Alouiscious Media <info@reneesoul.com>
2020-10-16 20:40:33 -0700

    revised talent actions

--- client/src/actions/talentActions.js
+++ client/src/actions/talentActions.js
@@ -1,6 +1,7 @@
+const TALENTURL = "http://localhost:3000/talents"
 
 export function addTalent(talent) {
-  // const talent = ({...this.state.talentForm, id: uuid()})
+  const talent = ({...this.state.talentInput, upid: upid})
   console.log('Talent from actions', talent)
 
   return (dispatch) => {
@@ -15,7 +16,7 @@ export function addTalent(talent) {
       })
     }
     
-    fetch("http://localhost:3000/talents", configTalent)
+    fetch(TALENTURL, configTalent)
     .then(resp => resp.json())
     .then(talentJSON => {
       if (talentJSON.error) {
@@ -30,25 +31,64 @@ export function addTalent(talent) {
   }
 }
 
+export const GET_TALENTS = 'GET TALENTS'
+export const GET_TALENTS_SUCCESS = 'GET_TALENTS_SUCCESS'
+export const GET_TALENTS_FAILURE = 'GET_TALENTS_FAILURE'
 
-export function getTalents(talents) {
-  return (dispatch) => {
-    fetch("http://localhost:3000/talents")
+export const fetchTalents = () => ({ type: GET_TALENTS })
+export const getTalentsFailure = () => ({ type: GET_TALENTS_FAILURE })
+
+export const getTalentsSuccess = talents => ({
+  type: GET_TALENTS_SUCCESS,
+  payload: talents,
+})
+
+
+export function getTalents(upid) {
+  return async dispatch => {
+    dispatch(fetchTalents())
+
+    return fetch(TALENTURL)
     .then(resp => resp.json())
     .then(talentsJSON => {
-      console.log('Talent from actions', talents)
+      console.log('talentsJSON from actions', talentsJSON)
       if (talentsJSON.error) {
+        dispatch(getTalentsFailure())
         alert("Talent List did not collect. Talent 41")
       } 
       else {
-        dispatch({ type: 'GET_TALENTS', talents: talentsJSON.talents})
+        dispatch(getTalentsSuccess(talentsJSON.talents))
       }
     })
-   
+    .catch(console.log)
+  }
+}
+
+export function removeTalent() {
+  return (dispatch) => {
+
+    const configTalent = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        talent: talent
+      })
+    }
+    
+    fetch(TALENTURL, configTalent)
+    .then(resp => resp.json())
+    .then(talentJSON => {
+      if (talentJSON.error) {
+        alert("Talent deletion was not complete.")
+      } 
+      else {
+        dispatch({ type: 'DELETE_TALENT', talent: talentJSON})
+        alert("Talent Removed.")
+      }
+    })
     .catch(console.log)   
   }
 
-
-}
-
-export default {addTalent, getTalents}
+// export default {addTalent, getTalents, removeTalent}
