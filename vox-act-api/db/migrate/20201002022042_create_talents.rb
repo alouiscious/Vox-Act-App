e03420c9353986cf50f636266b55617e99a7a7a8
Alouiscious Media <info@reneesoul.com>
2020-10-30 02:48:06 -0700

    added user foreign key to talents

--- vox-act-api/db/migrate/20201002022042_create_talents.rb
+++ vox-act-api/db/migrate/20201002022042_create_talents.rb
@@ -2,13 +2,13 @@ class CreateTalents < ActiveRecord::Migration[6.0]
   def change
     create_table :talents do |t|
       t.string :talent_style
-      t.string :user_name
-      t.string :upid
       t.string :title
       t.string :description
       t.string :phmf
       t.string :vimf
       t.string :aumf
+
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
