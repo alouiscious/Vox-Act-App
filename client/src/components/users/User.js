85f73aaede26d5d428c7da8c035e546e55f54a96
Alouiscious Media <info@reneesoul.com>
2020-12-22 03:37:35 -0800

    username links to useredit page

--- client/src/components/users/User.js
+++ client/src/components/users/User.js
@@ -5,12 +5,13 @@ import { Link } from "react-router-dom";
   const User = ({ user, talent }) => {
     return <div className={talent ? 'talent_style' : 'user'}>
       {console.table('wa ha user from user', user)}
-        <h3>{user.user_name} {user.hometown}</h3>
-          
-            <Link to={`/UserPage/${user.id}`}className="button">
-              View Client: {user.user_name}
-            </Link>
-          
+        <h3>
+          <Link to={`/UserEdit/${user.id}`}className="button">
+            {user.user_name} ~ {user.hometown}
+          </Link>
+        </h3>
+          Email: {user.email}
+          <br />
         {talent && talent ? user.email : user.upph}
     </div>  
   }
