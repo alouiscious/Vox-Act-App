839a9f37792fc59860c63c487c03bcfe45910d1d
Alouiscious Media <info@reneesoul.com>
2020-12-22 03:36:40 -0800

    added redirect router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import Users from './components/users/Users';
 import Navbar from "./components/Navbar";
 import VoxActSignIn from './containers/VoxActSignIn'
 import VoxActSignUp from './containers/VoxActSignUp'
-import User from './/components/users/User';
+// import User from './/components/users/User';
 import UserPage from './components/users/UserPage';
 import logo from './logo.jpg';
 import './App.css';
@@ -20,9 +20,9 @@ const App = () => {
           <Route exact path="/" component={VoxActSignIn} />
           <Route exact path="/UserInput" component={VoxActSignUp} />
           <Route exact path="/Users" component={Users} />
-          <Route exact path="/Users/:id" component={User} />
+          {/* <Route exact path="/Users/:id" component={User} /> */}
           <Route exact path="/UserPage/:id" component={UserPage} />
-          {/* <Redirect to="/" /> */}
+          <Redirect to="/" />
         </Switch>
       </Router>
 
